5f31ca2611c8f9e761ba238fdd35cd1bc24bcbe8
Andre Rendeiro <afrendeiro@gmail.com>
2026-08-24 10:53:41 +0100

    hyprland: make gaming workspace non-default (boot lands on workspace 1)

--- hyprland/.config/hypr/config/workspaces.lua
+++ hyprland/.config/hypr/config/workspaces.lua
@@ -1,6 +1,6 @@
 -- Workspace rules wiki https://wiki.hypr.land/Configuring/Basics/Workspace-Rules/
 -- Ten persistent workspaces on the primary monitor, so they always exist.
-hl.workspace_rule({ workspace = "name:gaming", monitor = PRIMARY_MONITOR, default = true })
+hl.workspace_rule({ workspace = "name:gaming", monitor = PRIMARY_MONITOR })
 for i = 1, 10 do
     hl.workspace_rule({ workspace = tostring(i), monitor = PRIMARY_MONITOR, default = true, persistent = true })
 end
